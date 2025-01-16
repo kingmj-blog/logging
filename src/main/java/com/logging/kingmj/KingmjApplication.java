901dd94410bb0abb86db55828356fc1204fa65f7
myeongju-kim <kingmj@daou.co.kr>
2025-01-16 15:21:21 +0900

    feat: init spring boot setting

---
+++ src/main/java/com/logging/kingmj/KingmjApplication.java
@@ -0,0 +1,13 @@
+package com.logging.kingmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingmjApplication.class, args);
+	}
+
+}
